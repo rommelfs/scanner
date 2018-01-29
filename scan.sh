580fa93fa1910690eab2692349ebb773fe099005
Sascha Rommelfangen <sascha@rommelfangen.de>
2018-01-29 10:15:31 +0100

    more flexibility

--- scan.sh
+++ scan.sh
@@ -6,8 +6,12 @@
 TARGET="$1"
 EMAIL="$2"
 INITIAL=false
-
-
+shift; shift
+NMAP_OPTIONS="$@"
+if [ -z "$NMAP_OPTIONS" ]
+then
+        NMAP_OPTIONS="$NMAP_DEFAULT_OPTIONS"
+fi
 
 # Check if the IP address is correct and containing a netmask
 echo $TARGET | egrep '^[0-9]{1,3}(\.[0-9]{1,3}){3}\/[0-9]{1,2}$'
@@ -21,32 +25,34 @@ fi
 echo $EMAIL | egrep '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}'
 if [ $? -gt 0 ]
 then
-	echo "$EMAIL is not a valid email address."
+	echo "$EMAIL doesn't seem to be a valid email address."
 	exit 1
 fi
 
-mkdir -p $BASEDIR/scans/$TARGET/{new,old,historic}
-FILENAME_NEW=$BASEDIR/scans/$TARGET/new/result.txt
-FILENAME_OLD=$BASEDIR/scans/$TARGET/old/result.txt
+mkdir -p "$BASEDIR/scans/$TARGET/$DIR_NEW"
+mkdir -p "$BASEDIR/scans/$TARGET/$DIR_OLD"
+mkdir -p "$BASEDIR/scans/$TARGET/$DIR_ARCHIVE"
+FILENAME_NEW=$BASEDIR/scans/$TARGET/$DIR_NEW/result.txt
+FILENAME_OLD=$BASEDIR/scans/$TARGET/$DIR_OLD/result.txt
 FILENAME_DIFF=$FILENAME_NEW.diff
-DIRECTORY_HISTORIC=$BASEDIR/scans/$TARGET/historic
+DIRECTORY_ARCHIVE=$BASEDIR/scans/$TARGET/$DIR_ARCHIVE
 
 # Check if we ran this before
 if [ -e $FILENAME_NEW ]
 then
         if [ -e $FILENAME_OLD ]
         then
-                mv $FILENAME_OLD $DIRECTORY_HISTORIC/scan-`date "+%Y%m%d-%s"`
+                mv $FILENAME_OLD $DIRECTORY_ARCHIVE/scan-`date "+%Y%m%d-%s"`
         fi
         mv $FILENAME_NEW $FILENAME_OLD
 else
         INITIAL=true
 fi
-# perform scan
 
+# perform scan
 if [ $INITIAL == true  ]
 then
-	nmap --open -n -T4 $TARGET -oX $FILENAME_NEW -oN $FILENAME_NEW.txt
+	$NMAP_BIN $NMAP_OPTIONS $TARGET -oX $FILENAME_NEW -oN $FILENAME_NEW.txt
         #cat $FILENAME_NEW.txt | mail -s "$EMAIL_SUBJECT_PREFIX $EMAIL_SUBJECT_INITIAL $TARGET" $EMAIL
         printf "$EMAIL_HEADER_INITIAL\n$EMAIL_BODY_INITIAL $TARGET:\n`cat $FILENAME_NEW.txt|grep -v 'Nmap'`\n\n$EMAIL_FOOTER_INITIAL\n$EMAIL_INFO" | \
                 mail -s "$EMAIL_SUBJECT_PREFIX $EMAIL_SUBJECT_INITIAL $TARGET" $EMAIL
@@ -54,7 +60,7 @@ then
 	rm $FILENAME_NEW.txt
         exit 0
 else
-	nmap --open -n -T4 $TARGET -oX $FILENAME_NEW
+	$NMAP_BIN $NMAP_OPTIONS $TARGET -oX $FILENAME_NEW
 fi
 
 # test if we have two files to compare
@@ -68,9 +74,9 @@ then
                 printf "$EMAIL_HEADER_CHANGE\n$EMAIL_BODY_CHANGE $TARGET:\n`cat $FILENAME_DIFF.mail`\n\n$EMAIL_FOOTER_CHANGE\n$EMAIL_INFO" | \
                     mail -s "$EMAIL_SUBJECT_PREFIX $EMAIL_SUBJECT_CHANGE $TARGET" $EMAIL
                 rm $FILENAME_DIFF.mail
-                FILENAME_DIFF_HISTORIC=$DIRECTORY_HISTORIC/diff-`date "+%Y%m%d-%s"`
-                mv $FILENAME_DIFF $FILENAME_DIFF_HISTORIC
-                logger "Sent mail to $EMAIL: network change detected for $TARGET. See file $FILENAME_DIFF_HISTORIC for details"
+                FILENAME_DIFF_ARCHIVE=$DIRECTORY_ARCHIVE/diff-`date "+%Y%m%d-%s"`
+                mv $FILENAME_DIFF $FILENAME_DIFF_ARCHIVE
+                logger "Sent mail to $EMAIL: network change detected for $TARGET. See file $FILENAME_DIFF_ARCHIVE for details"
         else
                 logger "No change detected for target $TARGET"
         fi
